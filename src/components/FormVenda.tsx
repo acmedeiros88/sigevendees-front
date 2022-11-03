2110204030ee2b7ab20e603630d4c704ab01a09d
Adriano Medeiros <adriano_cardoso@hotmail.com.br>
2022-11-03 20:54:47 -0300

    finalizado tela venda

---
+++ src/components/FormVenda.tsx
@@ -0,0 +1,154 @@
+import React from 'react';
+import {
+    Box,
+    HStack,
+    useColorModeValue,
+    SimpleGrid,
+    Table,
+    TableContainer,
+    Tbody,
+    Td,
+    Th,
+    Thead,
+    Tr
+} from '@chakra-ui/react';
+import {
+    InputNumber,
+    InputSelect,
+    ButtonSubmit,
+    InputText,
+    OptionItem
+} from '../components/forms/InputData';
+
+const Payments: Array<OptionItem> = [
+    { descricao: 'DINHEIRO', value: 0 },
+    { descricao: 'CREDITO', value: 1 },
+    { descricao: 'DEBITO', value: 2 }
+];
+
+function FormVenda() {
+    return (
+        <Box>
+            <HStack>
+                <SimpleGrid columns={12} columnGap={3} rowGap={6} minW='full'>
+                    <InputText
+                        descLabel='Cliente'
+                        columSpanBase={12}
+                        columSpanMD={12}
+                        columSpanLG={12}
+                        read_on={true}
+                    />
+                </SimpleGrid>
+            </HStack>
+
+            <HStack mt={4}>
+                <SimpleGrid columns={12} columnGap={3} rowGap={6} minW='full'>
+                    <InputSelect
+                        descLabel='Forma de pagamento'
+                        columSpanBase={12}
+                        columSpanMD={12}
+                        columSpanLG={7}
+                        options={Payments}
+                    />
+                    <InputNumber
+                        descLabel='Valor recebido'
+                        descPlaceholder='R$'
+                        columSpanBase={6}
+                        columSpanMD={6}
+                        columSpanLG={3}
+                    />
+                    <InputNumber
+                        descLabel='Valor desconto'
+                        descPlaceholder='R$'
+                        columSpanBase={6}
+                        columSpanMD={6}
+                        columSpanLG={2}
+                    />
+                </SimpleGrid>
+            </HStack>
+
+            <HStack mt={4}>
+                <SimpleGrid columns={12} columnGap={3} rowGap={6} minW='full'>
+                    <InputText
+                        descLabel='Subtotal'
+                        descPlaceholder='R$'
+                        columSpanBase={6}
+                        columSpanMD={6}
+                        columSpanLG={2}
+                        read_on={true}
+                    />
+                    <InputText
+                        descLabel='Descontos'
+                        descPlaceholder='R$'
+                        columSpanBase={6}
+                        columSpanMD={6}
+                        columSpanLG={2}
+                        read_on={true}
+                    />
+                    <InputText
+                        descLabel='Troco'
+                        descPlaceholder='R$'
+                        columSpanBase={6}
+                        columSpanMD={6}
+                        columSpanLG={3}
+                        read_on={true}
+                    />
+                    <InputText
+                        descLabel='Total a pagar'
+                        descPlaceholder='R$'
+                        columSpanBase={6}
+                        columSpanMD={6}
+                        columSpanLG={5}
+                        read_on={true}
+                    />
+                </SimpleGrid>
+            </HStack>
+
+            <TableContainer overflowY='auto' mt={8}>
+                <Table size='sm' variant='striped' bgColor={useColorModeValue('blackalpha.300', 'blackalpha.700')}>
+                    <Thead>
+                        <Tr>
+                            <Th>Código</Th>
+                            <Th>Produto</Th>
+                            <Th>Qtd</Th>
+                            <Th>Custo R$</Th>
+                            <Th>Desconto R$</Th>
+                            <Th>Subtotal R$</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        <Tr>
+                            <Td>1</Td>
+                            <Td>Bolo chocolate</Td>
+                            <Td>5</Td>
+                            <Td>R$ 18,00</Td>
+                            <Td>R$ 0</Td>
+                            <Td>R$ 90,00</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>2</Td>
+                            <Td>Brigadeiro</Td>
+                            <Td>1</Td>
+                            <Td>R$ 6,00</Td>
+                            <Td>R$ 2,00</Td>
+                            <Td>R$ 4,00</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>2</Td>
+                            <Td>Copo da felicidade</Td>
+                            <Td>10</Td>
+                            <Td>R$ 5,00</Td>
+                            <Td>R$ 0</Td>
+                            <Td>R$ 5,00</Td>
+                        </Tr>
+                    </Tbody>
+                </Table>
+            </TableContainer>
+
+            <ButtonSubmit margin_top={5} />
+
+        </Box>
+    );
+}
+
+export default FormVenda;
--- src/pages/venda.tsx
+++ src/pages/venda.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import FormVenda from '../components/FormVenda';
 import Template from '../components/layout/Template';
 
 function Venda() {
     return (
         <Template titulo='VENDA'>
+            <FormVenda />
         </Template>
     );
 }
